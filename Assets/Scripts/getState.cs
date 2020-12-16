404a2ed16da871d1383abb1e52e7baa8f8ef843d
drakitt <rakiterrazas@gmail.com>
2020-12-16 12:03:49 -0400

    Second Commit
    Se obuvo el estado de firebase

---
+++ Assets/Scripts/getState.cs
@@ -0,0 +1,56 @@
+﻿using Firebase.Database;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class getState : MonoBehaviour
+{
+    public class Firebasedata
+    {
+        public bool status;
+        public string  time;
+        
+        
+    }
+    public Firebasedata myData = new Firebasedata();
+    public GameObject[] lights = new GameObject[9];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject light in lights)
+        {
+            light.SetActive(myData.status);
+
+        }
+
+        FirebaseDatabase.DefaultInstance
+            .GetReference("historial")
+            .Child("med-001")
+            .ValueChanged += HandleValueChanged;
+        // 
+
+    }
+
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        DataSnapshot dataSnapshot = args.Snapshot;
+        Debug.Log(dataSnapshot.GetRawJsonValue());
+        //Toma los datos de Json
+        myData = JsonUtility.FromJson<Firebasedata>(dataSnapshot.GetRawJsonValue());
+        Debug.Log(myData.status);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        lights[0].SetActive(myData.status);
+        // foreach(GameObject light in lights)
+        // {
+        //     light.SetActive(myData.status);
+            
+        // }
+    }
+}
---
+++ Assets/Scripts/getTestDataFromFirebase.cs
@@ -0,0 +1,65 @@
+﻿using Firebase.Database;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getTestDataFromFirebase : MonoBehaviour
+{
+    
+    public GameObject lightObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightObject.SetActive(false);
+        Debug.Log("Initializing");
+        /*
+        FirebaseDatabase.DefaultInstance
+            .GetReference("medicine").Child("med-001")
+            .GetValueAsync().ContinueWith(task => {
+              
+                if (task.IsFaulted)
+                {
+                  // Handle the error...
+                }
+                else if (task.IsCompleted)
+                {
+                    DataSnapshot snapshot = task.Result;
+                    // Do something with snapshot...
+                    Debug.Log(snapshot.GetRawJsonValue());
+                }
+            });
+            */
+
+        
+        FirebaseDatabase.DefaultInstance
+            .GetReference("medicine").Child("med-001")
+            .ValueChanged += HandleValueChanged;
+            
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+        // Do something with the data in args.Snapshot
+
+        DataSnapshot dataSnapshot = args.Snapshot;
+        if (dataSnapshot != null)
+        {
+            lightObject.SetActive(true);
+            Debug.Log(dataSnapshot.GetRawJsonValue());
+        }
+        
+    }
+
+}
